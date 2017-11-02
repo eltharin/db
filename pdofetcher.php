97ea24fb8f9939ebab1bfa5be68bd9b6235a50dc
Roman <eltharin18@hotmail.fr>
2017-11-02 22:29:15 +0100

    1st commit

---
+++ pdofetcher.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DB;
+
+class pdofetcher
+{
+	private $stmt;
+	function __construct($stmt)
+	{
+		$this->stmt = $stmt;
+	}
+
+	function next($mode = \PDO::FETCH_ASSOC)
+	{
+		if ($this->stmt !== null)
+		{
+			return $this->stmt->fetch($mode);
+		}
+		return null;
+	}
+}
